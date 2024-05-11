6b05353f2e1768391b55aec65a87a9bdd5afa395
Martin Fixman <martinfixman@gmail.com>
2024-05-11 22:31:21 +0100

    Maybe preemptgpu is better.

--- Task 2/run_rink1.sh
+++ Task 2/run_rink1.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name rink                      # Job name
-#SBATCH --partition=prigpu                         # Select the correct partition.
+#SBATCH --partition=preemptgpu                         # Select the correct partition.
 #SBATCH --nodes=1                                # Run on 1 nodes (each node has 48 cores)
 #SBATCH --ntasks-per-node=1                        # Run one task
 #SBATCH --cpus-per-task=4                          # Use 4 cores, most of the procesing happens on the GPU
 #SBATCH --mem=72GB                                 # Expected ammount CPU RAM needed (Not GPU Memory)
 #SBATCH --time=72:00:00                            # Expected ammount of time to run Time limit hrs:min:sec
-#SBATCH --gres=gpu:1                               # Use one gpu.
+#SBATCH --gres=gpu:a100:1                               # Use one gpu.
 #SBATCH -e results/%x_%j.e                         # Standard output and error log [%j is replaced with the jobid]
 #SBATCH -o results/%x_%j.o                         # [%x with the job name], make sure 'results' folder exists.
 #SBATCH --error rink_1.err
 #SBATCH --output rink_1.out
+#SBATCH --constraint=cuda12
 
 #Enable modules command
 
